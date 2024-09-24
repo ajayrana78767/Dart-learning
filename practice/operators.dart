5df71fc24cf90c8c0beec2e7e28b99c510fd0902
ajayrana78767 <148841039+ajayrana78767@users.noreply.github.com>
2024-09-24 14:21:57 +0530

    Second Program

--- practice/operators.dart
+++ practice/operators.dart
@@ -1,5 +1,5 @@
 void main() {
-  // program 1 of compound operators
+  /* program 1 of compound operators
   int a = 10;
 
   print('Initial value of a: $a');
@@ -28,4 +28,19 @@ void main() {
   a = 10;
   a ~/= 3; // a = a ~/ 3
   print('After ~/= 3, a = $a');
+  */
+
+  // Program 2
+  int x = 20;
+  int y = 10;
+  int z = 5;
+
+  print('Initial values: x = $x, y = $y, z = $z');
+
+  // Using multiple compound operators on x
+  x += y; // x = x + y
+  x = 2; // x = x * 2
+  x %= 7; // x = x % 7
+
+  print('After modifications, x = $x'); // What will x be?
 }
